9fa63eb2c2f6746dc2b887562bb8af5f7994ffc9
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-02-05 00:15:12 +0200

    Improve ElementWithOneUsageInspection

--- src/main/kotlin/io/dragnea/usages/ElementWithOneUsageInspection.kt
+++ src/main/kotlin/io/dragnea/usages/ElementWithOneUsageInspection.kt
@@ -6,16 +6,11 @@ import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
-import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.PsiNameIdentifierOwner
+import com.intellij.psi.PsiParameter
+import com.intellij.psi.PsiTypeParameter
 import com.pythondce.inspections.usages
-import org.jetbrains.plugins.scala.lang.psi.impl.base.patterns.ScPatternArgumentListImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.base.patterns.ScPatternsImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.base.patterns.ScReferencePatternImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.expr.ScGeneratorImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.statements.ScFunctionDeclarationImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.statements.ScFunctionDefinitionImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.statements.params.ScParameterImpl
-import org.jetbrains.plugins.scala.lang.psi.impl.statements.params.ScTypeParamImpl
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScPattern
 
 class ElementWithOneUsageInspection : LocalInspectionTool() {
     override fun buildVisitor(
@@ -24,23 +19,16 @@ class ElementWithOneUsageInspection : LocalInspectionTool() {
         session: LocalInspectionToolSession
     ): PsiElementVisitor = object : PsiElementVisitor() {
         override fun visitElement(element: PsiElement) {
-            element is PsiNamedElement || return
+            if (element !is PsiNameIdentifierOwner) return
 
-            element::class in listOf(ScParameterImpl::class, ScTypeParamImpl::class) && return
+            element is PsiParameter && return
+            element is PsiTypeParameter && return
 
-            element is ScReferencePatternImpl && element.parent::class in listOf(
-                ScGeneratorImpl::class,
-                ScPatternsImpl::class,
-                ScPatternArgumentListImpl::class
-            ) && return
+            element is ScPattern && return
 
             if (element.usages().size == 1) {
                 holder.registerProblem(
-                    when (element) {
-                        is ScFunctionDefinitionImpl<*> -> element.nameId()
-                        is ScFunctionDeclarationImpl -> element.nameId()
-                        else -> element
-                    },
+                    element.nameIdentifier!!,
                     "Element has only one usage and can be inlined",
                     ProblemHighlightType.GENERIC_ERROR_OR_WARNING
                 )
