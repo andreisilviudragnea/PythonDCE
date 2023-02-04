ffdb59f161d4ff5e3b9705c6502dbfdd6038714e
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-02-05 01:16:07 +0200

    Refine Scala results

--- src/main/kotlin/io/dragnea/usages/ElementWithOneUsageInspection.kt
+++ src/main/kotlin/io/dragnea/usages/ElementWithOneUsageInspection.kt
@@ -4,6 +4,7 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiNameIdentifierOwner
@@ -11,7 +12,12 @@ import com.intellij.psi.PsiParameter
 import com.intellij.psi.PsiTypeParameter
 import com.pythondce.inspections.usages
 import org.jetbrains.kotlin.psi.KtParameter
-import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScPattern
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScNamingPattern
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScReferencePattern
+import org.jetbrains.plugins.scala.lang.psi.api.base.patterns.ScTypedPattern
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScGenerator
+import org.jetbrains.plugins.scala.lang.psi.api.statements.params.ScArguments
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTrait
 
 class ElementWithOneUsageInspection : LocalInspectionTool() {
     override fun buildVisitor(
@@ -25,12 +31,17 @@ class ElementWithOneUsageInspection : LocalInspectionTool() {
             element is PsiParameter && return
             element is KtParameter && return // KtParameter is not PsiParameter
             element is PsiTypeParameter && return
+            element is PsiClass && return
 
-            element is ScPattern && return
+            element.parent?.parent?.parent is ScTrait && return // Skip methods from traits
+            element is ScTypedPattern && return
+            element is ScReferencePattern && element.parent is ScGenerator && return
+            element is ScReferencePattern && element.parent is ScArguments && return
+            element is ScNamingPattern && return
 
             if (element.usages().size == 1) {
                 holder.registerProblem(
-                    element.nameIdentifier!!,
+                    element.nameIdentifier ?: return,
                     "Element has only one usage and can be inlined",
                     ProblemHighlightType.GENERIC_ERROR_OR_WARNING
                 )
